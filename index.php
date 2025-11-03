1b7a4b8d9dccec14810dc6414841e3f9bbaeb19a
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-11-03 09:36:17 +0100

    tableau associatif fait

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exos php novembre 2025</title>
 </head>
+
 <body>
     <?php
-        echo "Hello World";
+    // echo "Hello World";
+    $user = [
+        "email" => "billy@gmail.com",
+        "name" => "Billy",
+        "lastname"   => "Joe",
+        "img"  => "http://unsplash/100/100",
+    ];
+
+    var_dump($user);
     ?>
 </body>
+
 </html>
